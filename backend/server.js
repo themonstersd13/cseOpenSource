d242e479e366d73213de88144c6609870af30522
saurabh_doiphode <saurabhdoiphode1335@gmail.com>
2024-07-11 12:44:17 +0530

    login/register controllers added

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,39 @@ db.on("error", () => console.log("error"));
 db.once("open", () => console.log("successful"));
 
 
+app.post('/register',(req,res)=>{
+  const {username,password} =req.body;
+  const data={
+    username:username,
+    password:password,
+    arr:[]
+  }
+  db.collection("users").insertOne(data, (err, collection) => {
+    if (err) {
+      res.json({message:err});
+    }
+    console.log("done data successfully ");
+    res.json({message:"successful"});
+  });
+})
+
+app.post('/login',(req,res)=>{
+  const {username,password} = req.body;
+  const data={
+    username:username,
+    password:password
+  }
+  db.collection('users').findOne(data)
+  .then((result)=>{
+    res.status(200).json({message:"successfull",arr:result.arr});
+  })
+  .catch((err)=>{
+    console.log(err);
+    res.status(404).json({message:"invalid"});
+  })
+})
+
+
 app.post('/upload', (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({ error: 'No file uploaded.' });
@@ -27,10 +60,10 @@ app.post('/upload', (req, res) => {
   const currentId = req.body.currentId;
   const file = req.files.file;
   const fileName=`${req.body.filename}${path.extname(file.name)}`;
-  // Set destination path dynamically
+  
   const uploadPath = `E:/PROGRAMING/webD/openSourceWceCse/frontend/public/data/${currentId}/${fileName}`;
 
-  // Use mv() method to place file on server
+  
   file.mv(uploadPath, (err) => {
     if (err) {
       return res.status(500).json({ error: err.message });
@@ -61,7 +94,6 @@ app.post('/upload', (req, res) => {
         console.error('Database query failed', err);
         res.status(500).json({ error: 'Internal Server Error' });
       });
-    // res.json({ currentId: currentId, fileName });
   });
   
 });
@@ -90,29 +122,6 @@ db.collection("DomainData").findOne({idName:currentId})
       res.status(500).json({ error: 'Internal Server Error' });
     });
 })
-
-app.post("/updatedata", (req, res) => {
-  const {currentId,dataVector}=req.body;
-  console.log(currentId);
-  db.collection("DomainData").findOne({idName:currentId})
-  .then((obs)=>{
-      if (obs && obs.arr) {
-        db.collection("DomainData").updateOne(
-          {currentId},
-          { $set: { arr: dataVector } }
-        )
-        console.log('Result:', obs.arr);
-        res.json(obs.arr);
-        } else {
-          console.log('No data found');
-          res.status(404).json({ error: 'No data found' });
-        }
-      })
-      .catch((err) => {
-        console.error('Database query failed', err);
-        res.status(500).json({ error: 'Internal Server Error' });
-      });
-  })
 
   app.listen(3500, () => {
     console.log('Server is running on port 3500');
