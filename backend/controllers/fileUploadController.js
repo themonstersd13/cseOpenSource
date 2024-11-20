ce3fd2d16ee40e8e0e7fb09a657bef9a5e8f4f62
themonstersd13 <saurabhdoiphode1335@gmail.com>
2024-11-21 01:12:05 +0530

    1.1.1.7

--- backend/controllers/fileUploadController.js
+++ backend/controllers/fileUploadController.js
@@ -1,21 +1,27 @@
-const DomainData = require('../models/DomainData'); // Import the DomainData model
-
+const path = require('path');
+const DomainData = require('../models/DomainData'); 
+const { put } = require("@vercel/blob");
 exports.uploadFile = async (req, res) => {
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({ error: 'No file uploaded.' });
   }
 
+  console.log("agaya");
+
   const currentId = req.body.currentId;
   const file = req.files.file;
+
   const fileName = `${req.body.filename}${path.extname(file.name)}`;
 
   try {
     const { url } = await put(fileName, file.data, { access: 'public' });
+
     const obs = await DomainData.findOne({ idName: currentId });
 
     if (obs && obs.arr && obs.titleArr) {
       let dataVector = obs.arr;
       let titleVector = obs.titleArr;
+
       titleVector.push(fileName);
       dataVector.push(url);
 
@@ -29,11 +35,18 @@ exports.uploadFile = async (req, res) => {
       const newData = new DomainData({
         idName: currentId,
         arr: [url],
-        titleArr: [fileName]
+        titleArr: [fileName],
       });
-      await newData.save(); 
 
-      res.json({ message: "successful", currentId, fileName, dataVector: [url], titleArr: [fileName] });
+      await newData.save();
+
+      res.json({
+        message: "successful",
+        currentId,
+        fileName,
+        dataVector: [url],
+        titleArr: [fileName],
+      });
     }
   } catch (err) {
     console.error('File upload failed', err);
